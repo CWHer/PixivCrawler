dad5ed31476b7bffc720d86bf715fd0fc2761d07
cwher <cwher@outlook.com>
2022-05-11 14:44:03 +0800

    test: add collector test

---
+++ tests/test_collector.py
@@ -0,0 +1,8 @@
+from collector.collector import Collector
+from downloader.downloader import Downloader
+
+
+downloader = Downloader(capacity=1024)
+collector = Collector(downloader)
+collector.add(("89362167", "85275328"))
+collector.collect()
