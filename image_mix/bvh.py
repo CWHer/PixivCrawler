faf0a147ffed3584b7ffd9abef80d8e12b915a90
cwher <cwher@outlook.com>
2022-05-11 23:26:15 +0800

    perf: upd BVH tree

--- image_mix/bvh.py
+++ image_mix/bvh.py
@@ -1,166 +1,191 @@
+from __future__ import annotations
+
 import math
 import random
-import time
+from typing import List, Optional
+
+from utils import printError
 
 
-def sqr(x):
-    return x * x
-
-
-# norm=2 dist
-#   between box and pos
-def dist(a, b):
-    d2 = 0
-    for i in range(3):
-        l = a.mn.pos[i]
-        r = a.mx.pos[i]
-        if l <= b.pos[i] and b.pos[i] <= r: continue
-        d2 += min(sqr(l - b.pos[i]), sqr(r - b.pos[i]))
-    return math.sqrt(d2)
-
-
-class Pos():
+class Point():
     def __init__(self, x, y, z):
         self.pos = [x, y, z]
 
+    @property
     def x(self):
         return self.pos[0]
 
+    @property
     def y(self):
         return self.pos[1]
 
+    @property
     def z(self):
         return self.pos[2]
 
 
 class Box():
-    # mn=mx at leaf node
-    def __init__(self, mn, mx):
-        self.mn = mn
-        self.mx = mx
+    def __init__(self, min_p: Point, max_p: Point):
+        # NOTE: max_p == min_p at leaf node
+        self.min_p = min_p
+        self.max_p = max_p
+
+    @staticmethod
+    def boundingBox(a: Box, b: Box) -> Box:
+        min_p = Point(min(a.min_p.x, b.min_p.x),
+                      min(a.min_p.y, b.min_p.y),
+                      min(a.min_p.z, b.min_p.z))
+        max_p = Point(max(a.max_p.x, b.max_p.x),
+                      max(a.max_p.y, b.max_p.y),
+                      max(a.max_p.z, b.max_p.z))
+        return Box(min_p, max_p)
+
+    def dist(self, point: Point) -> float:
+        """[summary]
+        norm = 2 dist, between box and pos
+        """
+        def sqr(x):
+            return x * x
+
+        d2 = 0
+        for i in range(3):
+            low = self.min_p.pos[i]
+            high = self.max_p.pos[i]
+            if low <= point.pos[i] \
+                    and point.pos[i] <= high:
+                continue
+            d2 += sqr(low - point.pos[i]) \
+                if point.pos[i] <= low else sqr(high - point.pos[i])
+        return math.sqrt(d2)
 
 
-# bounding_box of 2 Box
-def bounding_box(a, b):
-    mn = Pos(min(a.mn.x(), b.mn.x()), min(a.mn.y(), b.mn.y()),
-             min(a.mn.z(), b.mn.z()))
-    mx = Pos(max(a.mx.x(), b.mx.x()), max(a.mx.y(), b.mx.y()),
-             max(a.mx.z(), b.mx.z()))
-    return Box(mn, mx)
-
-
-class BVHnode():
-    def __init__(self):
-        self.box = None
-        self.ch = []
-        self.fa = None
+class BVHNode():
+    def __init__(self, father):
         self.used_times = 0
+        self.father = father
+        self.box: Optional[Box] = None
+        self.child: List[BVHNode] = []
 
 
 class BVH():
-    def __init__(self):
-        random.seed(time.time())
-        self.nodes = []
-        self.cnt = 0
-        self.dist = None
-        self.ans = None
+    def __init__(self, MAX_TIMES: int):
+        random.seed(0)
+        self.root: Optional[BVHNode] = None
+        self.nodes: List[BVHNode] = []
+        self.MAX_TIMES = MAX_TIMES
+        self.reset()
 
-    # list of box
-    def build(self, fa, boxes):
-        num = len(boxes)
-        self.nodes.append(BVHnode())
-        now_id = self.cnt
-        self.cnt += 1
-        self.nodes[now_id].fa = fa
-        if not fa == None: self.nodes[fa].ch.append(now_id)
-        if num == 1:
-            self.nodes[now_id].box = Box(boxes[0], boxes[0])
+    def reset(self):
+        # NOTE: query answer
+        self.dist: float = float("inf")
+        self.ans: Optional[BVHNode] = None
+
+    def build(self,
+              father: Optional[BVHNode],
+              points: List[Point]) -> None:
+        n_box = len(points)
+        current = BVHNode(father)
+        self.nodes.append(current)
+        if father is not None:
+            father.child.append(current)
+        else:
+            self.root = current
+
+        if n_box == 1:
+            current.box = Box(points[0], points[0])
             return
-        if num == 2:
-            self.build(now_id, boxes[0:1])
-            self.build(now_id, boxes[-1:num])
-            ls = self.nodes[now_id].ch[0]
-            rs = self.nodes[now_id].ch[1]
-            self.nodes[now_id].box = bounding_box(self.nodes[ls].box,
-                                                  self.nodes[rs].box)
+        if n_box == 2:
+            self.build(current, [points[0]])
+            self.build(current, [points[-1]])
+            current.box = Box.boundingBox(
+                current.child[0].box, current.child[1].box)
             return
+
         axis = random.randint(0, 2)
-        boxes.sort(key=lambda x: x.pos[axis])
-        mid = num // 2
-        self.build(now_id, boxes[0:mid])
-        self.build(now_id, boxes[mid:num])
-        ls = self.nodes[now_id].ch[0]
-        rs = self.nodes[now_id].ch[1]
-        self.nodes[now_id].box = bounding_box(self.nodes[ls].box,
-                                              self.nodes[rs].box)
+        points.sort(key=lambda x: x.pos[axis])
+        mid = n_box // 2
+        self.build(current, points[0:mid])
+        self.build(current, points[mid:n_box])
+        current.box = Box.boundingBox(
+            current.child[0].box, current.child[1].box)
 
-    # find closest of Pos q
-    #   store ans in self.ans
-    def query(self, o, q):
-        if len(self.nodes[o].ch) == 0:
-            # if self.nodes[o].used_times >= self.REPEAT_TIMES:
-            #     # self.nodes[fa]
-            #     return
-            dis = dist(self.nodes[o].box, q)
-            if dis < self.dist:
-                self.dist = dis
-                self.ans = o
+    def query(self, q: Point,
+              x: Optional[BVHNode] = None) -> None:
+        """[summary]
+        find the point in the tree, that is closest to a given q
+        """
+        if x is None:
+            x: BVHNode = self.root
+
+        if len(x.child) == 0:
+            if x.used_times >= \
+                    self.MAX_TIMES:
+                return
+            dist = x.box.dist(q)
+            if dist < self.dist:
+                self.dist = dist
+                self.ans = x
             return
 
-        ls = self.nodes[o].ch[0]
-        rs = self.nodes[o].ch[1]
-        ld = dist(self.nodes[ls].box, q)
-        # avoid searching same node
-        rd = (1 << 30) if ls == rs else dist(self.nodes[rs].box, q)
-        if ld <= rd:
-            if ld < self.dist: self.query(ls, q)
-            if rd < self.dist: self.query(rs, q)
+        children = [
+            (x.child[i], x.child[i].box.dist(q))
+            for i in range(2)]
+        children.sort(key=lambda x: x[-1])
+        for child, dist in children:
+            if dist < self.dist:
+                self.query(q, child)
+
+    def remove(self, x: BVHNode):
+        """[summary]
+        x must be a leaf node
+        """
+        # NOTE: replace x.father with x.brother
+        y: BVHNode = x.father
+        printError(
+            y is None, "BVH tree is empty")
+        z: BVHNode = y.father
+        t = y.child[y.child[0] == x]
+        t.father = z
+        if z is not None:
+            k = z.child[1] == y
+            z.child[k] = t
         else:
-            if rd < self.dist: self.query(rs, q)
-            if ld < self.dist: self.query(ls, q)
+            self.root = t
 
-    # f=1 remove/ f=0 modify
-    def remove(self, o, f):
-        fa = self.nodes[o].fa
-        if fa == None: return
-        if f == 0:
-            ls = self.nodes[fa].ch[0]
-            rs = self.nodes[fa].ch[1]
-            self.nodes[fa].box = bounding_box(self.nodes[ls].box,
-                                              self.nodes[rs].box)
-            self.remove(fa, 0)
-            return
-        # f==1
-        if self.nodes[fa].ch[0] != self.nodes[fa].ch[1]:
-            k = self.nodes[fa].ch[1] == o
-            self.nodes[fa].ch[k] = self.nodes[fa].ch[k ^ 1]
-            self.nodes[fa].box = self.nodes[self.nodes[fa].ch[k]].box
-            self.remove(fa, 0)
-        else:
-            self.remove(fa, 1)
+        current = z
+        while current is not None:
+            current.box = Box.boundingBox(
+                current.child[0].box, current.child[1].box)
+            current = current.father
 
 
-# # debug begin
-# group = [
-#     Pos(random.random(), random.random(), random.random()) for i in range(100)
-# ]
-# t = BVH(0)
-# t.build(None, group)
-# print(t.nodes[0].box.mn.pos)
-# t.dist = 10000
-# ask = Pos(random.random(), random.random(), random.random())
-# t.query(0, ask)
-# print(t.dist)
-# ans = t.nodes[t.ans].box.mx
-# print("({}, {}, {})".format(ans.pos[0], ans.pos[1], ans.pos[2]))
-# t.remove(t.ans, 1)
-# t.dist = 10000
-# t.query(0, ask)
-# print(t.dist)
-# ans = t.nodes[t.ans].box.mx
-# print("({}, {}, {})".format(ans.pos[0], ans.pos[1], ans.pos[2]))
-# d = 10000
-# for dot in group:
-#     d = min(dist(Box(dot, dot), ask), d)
-# print(d)
-# # debug end
+if __name__ == "__main__":
+    # debug
+    def genPoint():
+        return Point(random.random(),
+                     random.random(),
+                     random.random())
+
+    bvh_tree = BVH(MAX_TIMES=1)
+    points = [genPoint() for _ in range(100)]
+    bvh_tree.build(father=None, points=points)
+    print(bvh_tree.root.box.min_p.pos, bvh_tree.root.box.max_p.pos)
+
+    q = genPoint()
+    bvh_tree.query(q)
+    print(bvh_tree.dist)
+    ans = bvh_tree.ans.box.max_p
+    print(f"({ans.x}, {ans.y}, {ans.z})")
+    bvh_tree.remove(bvh_tree.ans)
+    # t.ans.used_times += 1
+
+    bvh_tree.reset()
+    bvh_tree.query(q)
+    print(bvh_tree.dist)
+    ans = bvh_tree.ans.box.max_p
+    print(f"({ans.x}, {ans.y}, {ans.z})")
+
+    distances = [
+        Box(point, point).dist(q) for point in points]
+    distances.sort()
+    print(distances[:5])
